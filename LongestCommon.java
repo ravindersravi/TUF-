ec93f0b9a12b3e529324dcb3e5614f054e35d96d
Ravi <62902720+ravindersravi@users.noreply.github.com>
2025-01-23 21:22:16 +0530

    Update LongestCommon.java

--- LongestCommon.java
+++ LongestCommon.java
@@ -1,1 +1,14 @@
+https://www.youtube.com/watch?v=wuL8a7bStFg
 
+Length of longest Prefix Suffix
+
+  S="abab"  o/p: 2
+  "ab" is longest proper prefix and suffix
+
+   proper prefix: any prefix which is not equal to length of the string
+  here 4 a's are prefix but proper prefix is 3.
+  s="aaaa"  o/p: 3
+  "aaa" is longest prefix & suffix
+
+  ------------------
+  s=""
